e5c2953503ad675fb92bd18a0940aa05ba49c0d2
Paul Gomez <pauldro952@outlook.com>
2019-03-27 14:49:58 -0500

    Filter commits

--- site/templates/repository-commits.php
+++ site/templates/repository-commits.php
@@ -1,13 +1,71 @@
 <?php
+	include($config->paths->templates."/twig/repositories/commits/functions.php");
     $repository = $page->parent;
 
 	$github = $modules->get('GitHubConnector');
 	$github->import_commits($repository);
+	$customers = $repository->child('name=customers')->children();
+	$selector = '';
 
-    $customers = $repository->child('name=customers')->children();
+	$fields_toinputs = array(
+		'date' => array(
+			array(
+				'input'     => 'date',
+				'field'     => 'date',
+				'valuetype' => 'array',
+				'delimiter' => '|',
+				'operator'  => '>=|<='
+			),
+			array(
+				'input'     => 'after',
+				'field'     => 'date',
+				'valuetype' => 'single',
+				'operator'  => '>'
+			),
+			array(
+				'input'     => 'before',
+				'field'     => 'date',
+				'valuetype' => 'single',
+				'operator'  => '<'
+			)
+		),
+	);
 
-    include($config->paths->templates."/twig/repositories/commits/functions.php");
+	if ($input->get->after || $input->get->before) {
+		if ($input->get->after) {
+			$sha = $input->get->text('after');
 
-    $page->body = $config->twig->render('repositories/commits/commits-table.twig', ['commits' => $page->find('sort=-date'), 'customers' => $customers]);
+			if ($page->hasChildren("name=$sha")) {
+				$aftercommit = $page->get("name=$sha");
+				$input->get->after = date('m/d/Y', strtotime($aftercommit->date));
+			} else {
+				$input->get->after = '';
+			}
+		}
+
+		if ($input->get->before) {
+			$sha = $input->get->text('before');
+
+			if ($page->hasChildren("name=$sha")) {
+				$beforecommit = $page->get("name=$sha");
+				$input->get->before = date('m/d/Y', strtotime($beforecommit->date));
+			} else {
+				$input->get->before = '';
+			}
+		}
+	}
+
+	$selector .= ",".$modules->get('SelectorsFilter')->build_selectorstring($input, 'repository-commit', $fields_toinputs);
+
+
+	$commits = $page->children($selector);
+	$selectors = $commits->getSelectors();
+
+	if ($selectors->is_filtering('repository-commit')) {
+		$page->title = "Commits with " . $selectors->describe('repository-commit');
+	}
+
+
+    $page->body = $config->twig->render('repositories/commits/commits-table.twig', ['commits' => $commits, 'customers' => $customers]);
     $config->scripts->append(get_hashedtemplatefileurl('scripts/pages/repository.js'));
     include __DIR__ . "/basic-page.php";
