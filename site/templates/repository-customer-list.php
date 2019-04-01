5628a40f78b87d8bea3b00e93ccaaa3a125bcc6b
bbullemer <barbara.bullemer@gmail.com>
2019-04-01 11:14:15 -0500

    Back Buttons
    - added $page on render function for customers-table.twig file
    - added back button to commits-table and customers-table

--- site/templates/repository-customer-list.php
+++ site/templates/repository-customer-list.php
@@ -2,6 +2,6 @@
 	$customers = $page->children();
 	$module_repo = $modules->get('GitHubRepositories');
 	$page->title = "{$page->parent->repocode} Customers";
-	$page->body = $config->twig->render('repositories/customers/customers-table.twig', ['customers' => $customers, 'module_repo' => $module_repo]);
+	$page->body = $config->twig->render('repositories/customers/customers-table.twig', ['page' => $page, 'customers' => $customers, 'module_repo' => $module_repo]);
 	$config->scripts->append(get_hashedtemplatefileurl('scripts/pages/repository.js'));
 	include __DIR__ . "/basic-page.php";
