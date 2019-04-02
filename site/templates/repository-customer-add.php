4fd2d95adad8ffadea880334ec8ffc1cf2f3b637
bbullemer <barbara.bullemer@gmail.com>
2019-04-02 12:36:57 -0500

    Adding Customer
    - added if statement to repository-customer-add template to check if customer exists and then add - success or error message displays
    - changed $customers to look for template repository-customer because an error was popping up because of another page that is a child
    - created form to add customers
    - added button on customer table to add customer

--- site/templates/repository-customer-add.php
+++ site/templates/repository-customer-add.php
@@ -1,5 +1,26 @@
 <?php
-    $page->body = $config->twig->render('repositories/customers/customer-add.twig', ['customer' => $page, 'commits' => $commits]);
+    $module_repo = $modules->get('GitHubRepositories');
+
+    $customers = $page->siblings('template=repository-customer');
+
+    if ($input->requestMethod('POST')) {
+        $custtitle = $input->post->text('custtitle');
+        $custrepo = $input->post->text('custrepo');
+        $custcode = $input->post->text('custcode');
+        $custnote = $input->post->text('custnote');
+
+        $does_exist = $customers->find("repo=$custrepo")->count;
+
+        if ($does_exist) {
+            $success = false;
+        } else {
+            $success = $module_repo->add_customer($custtitle, $custrepo, $custcode, $custnote);
+        }
+    }
+
+    $inputpost = $input->requestMethod('POST');
+
+    $page->body = $config->twig->render('repositories/customers/customer-add.twig', ['page' => $page, 'customer' => $page, 'commits' => $commits, 'inputpost' => $inputpost, 'success' => $success, 'custtitle' => $custtitle]);
     $config->scripts->append(get_hashedtemplatefileurl('scripts/pages/repository.js'));
 
     include __DIR__ . "/basic-page.php";
--- site/templates/repository-customer-list.php
+++ site/templates/repository-customer-list.php
@@ -1,5 +1,5 @@
 <?php
-	$customers = $page->children();
+	$customers = $page->children('template=repository-customer');
 	$module_repo = $modules->get('GitHubRepositories');
 	$page->title = "{$page->parent->repocode} Customers";
 	$page->body = $config->twig->render('repositories/customers/customers-table.twig', ['page' => $page, 'customers' => $customers, 'module_repo' => $module_repo]);
