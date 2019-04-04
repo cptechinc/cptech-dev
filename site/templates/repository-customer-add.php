9537c11ce1dd09b7fe43dc3f0ba9db72ab01190a
bbullemer <barbara.bullemer@gmail.com>
2019-04-04 11:58:52 -0500

    #4 Customer Add Error
    - moved error message to top
    - checks to see if input for title is empty, if it is displays error message to enter title

--- site/templates/repository-customer-add.php
+++ site/templates/repository-customer-add.php
@@ -4,23 +4,28 @@
     $customers = $page->siblings('template=repository-customer');
 
     if ($input->requestMethod('POST')) {
-        $custtitle = $input->post->text('custtitle');
-        $custrepo = $input->post->text('custrepo');
-        $custcode = $input->post->text('custcode');
-        $custnote = $input->post->text('custnote');
+        if (!empty($input->post->text('custtitle'))) {
+            $custtitle = $input->post->text('custtitle');
+            $custrepo = $input->post->text('custrepo');
+            $custcode = $input->post->text('custcode');
+            $custnote = $input->post->text('custnote');
 
-        $does_exist = $customers->find("repo=$custrepo")->count;
+            $does_exist = $customers->find("repo=$custrepo")->count;
 
-        if ($does_exist) {
-            $success = false;
+            if ($does_exist) {
+                $success = false;
+            } else {
+                $success = $module_repo->add_customer($custtitle, $custrepo, $custcode, $custnote);
+            }
         } else {
-            $success = $module_repo->add_customer($custtitle, $custrepo, $custcode, $custnote);
+            $success = false;
+            $errormsg = 'Please enter a Customer Title';
         }
     }
 
     $inputpost = $input->requestMethod('POST');
 
-    $page->body = $config->twig->render('repositories/customers/customer-add.twig', ['page' => $page, 'customer' => $page, 'commits' => $commits, 'inputpost' => $inputpost, 'success' => $success, 'custtitle' => $custtitle]);
+    $page->body = $config->twig->render('repositories/customers/customer-add.twig', ['page' => $page, 'customer' => $page, 'commits' => $commits, 'inputpost' => $inputpost, 'success' => $success, 'errormsg' => $errormsg, 'custtitle' => $custtitle]);
     $config->scripts->append(get_hashedtemplatefileurl('scripts/pages/repository.js'));
 
     include __DIR__ . "/basic-page.php";
