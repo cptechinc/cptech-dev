19043f24e766c3b02cae2c82aed14d7356d4507c
Paul Gomez <pauldro952@outlook.com>
2019-07-11 15:12:32 -0500

    Update tool-dplusdb.php

--- site/templates/tool-dplusdb.php
+++ site/templates/tool-dplusdb.php
@@ -1,6 +1,7 @@
-<?php 
-    $user = $session->forceLogin('rcapsys');
-    $dplusdatabase = $modules->get('DplusDatabase');
+<?php
+	$database_dplus = $modules->get('DplusConnectDatabase');
+	$database_dpluso = $modules->get('DplusOnlineDatabase');
+
 ?>
 <?php include('./_head.php'); ?>
 	<main role="main">
@@ -10,23 +11,45 @@
 			</div>
 		</div>
 		<div class="container page">
-            <?= $page->body; ?>
-            <h2>Database Table SQL</h2>
-            
-			<div class="pl-5 pr-5 code-div">
-                <div class="row">
-                    <div class="col">
-                        <div class="form-group">
-                            <button type="button" class="btn btn-primary btn-sm pull-right" onclick="selectText('db-tables')">Copy</button>
-                        </div>
-                    </div>
-                </div>
-                
-                
-                <code id="db-tables">
-                    <?= $dplusdatabase->get_dbtablestructure() ?>
-                </code>
-            </div>
+			<?= $page->body; ?>
+			<h2>Database Table SQL</h2>
+
+			<div class="mb-3">
+				<h3>Dplus Data</h3>
+				<div class="pl-5 pr-5 code-div">
+					<div class="row">
+						<div class="col">
+							<div class="form-group">
+								<button type="button" class="btn btn-primary btn-sm pull-right" onclick="selectText('db-tables')">Copy</button>
+							</div>
+						</div>
+					</div>
+
+
+					<code id="db-tables">
+						<?= $database_dplus->get_dbtablestructure() ?>
+					</code>
+				</div>
+			</div>
+
+			<div class="mb-3">
+				<h3>Dplus Online</h3>
+				<div class="pl-5 pr-5 code-div">
+					<div class="row">
+						<div class="col">
+							<div class="form-group">
+								<button type="button" class="btn btn-primary btn-sm pull-right" onclick="selectText('db-tables2')">Copy</button>
+							</div>
+						</div>
+					</div>
+
+
+					<code id="db-tables2">
+						<?= $database_dpluso->get_dbtablestructure(); ?>
+					</code>
+				</div>
+			</div>
+
 		</div>
 	</main>
 <?php include('./_foot.php'); // include footer markup ?>
